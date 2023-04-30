1a985ce1661dbd995d7f77ce0dff243ec2c7978f
steven <126836450+Bpipy@users.noreply.github.com>
2023-04-30 14:16:42 -0400

    Second commit

---
+++ HypotenuseCalculator.py
@@ -0,0 +1,23 @@
+### Hypotenuse Calculator program
+import time
+import math
+
+print("#####Hypotenuse Calculator#####")
+
+height = input("Please input your triangles height!")
+
+width = input("Please input your trianles with!")
+
+print("Calculating.")
+time.sleep(0.1)
+
+print("Calculating..")
+time.sleep(0.1)
+
+print("Calculating...")
+time.sleep(0.1)
+
+height = int(height)
+width = int(width)
+
+print(math.sqrt((height * height) + (width * width)))
